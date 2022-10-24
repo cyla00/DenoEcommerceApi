bca9cc6de07952a43c9660901056f1d2ff3d0add
cyla00 <ikhayam000@protonmail.com>
2022-10-24 18:20:27 +0200

    interfaces base data

--- src/database/interfaces.ts
+++ src/database/interfaces.ts
@@ -1,20 +1,90 @@
+import { Timestamp } from "https://deno.land/x/web_bson@v0.2.5/mod.ts";
 
+// admin management
+export interface AdminSchema {
+    id: string,
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    roleId: Array<string>,
+    createdAt: Timestamp,
+    lastLogin: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
+}
 
-// application
+export interface RoleSchema {
+    id: string,
+    roleName: string,
+}
+
+// client management
 export interface UserSchema {
     id: string,
-    username: string,
+    firstName: string,
+    lastName: string,
+    email: string,
     password: string,
+    adresses: Array<object>,
+    createdAt: Timestamp,
+    lastLogin: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
+}
+
+export interface OrderSchema {
+    id: string,
+    userId: string,
+    productId: string,
+    quantity: number,
+    createdAt: Timestamp,
+    modifiedAt: Timestamp,
+}
+
+
+
+// product management
+export interface ProductSchema {
+    id: string,
+    productName: string,
+    categoryId: string,
+    brandId: string,
+    tags: Array<string>,
+    price: number,
+    createdAt: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
+}
+
+export interface InventorySchema {
+    id: string,
+    quantity: number,
+    createdAt: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
 }
 
 export interface CategoriesSchema {
     id: string,
+    categoryName: string,
+    createdAt: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
 }
 
 export interface TagSchema {
     id: string,
+    tagName: string,
+    createdAt: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
 }
 
-export interface ProductSchema {
+export interface BrandsSchema {
     id: string,
+    brandName: string,
+    createdAt: Timestamp,
+    modifiedAt: Timestamp,
+    deletedAt: Timestamp,
 }
